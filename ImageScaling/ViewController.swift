56a963c21793796bf4824de5ddf74211064acb4d
Suraphan Laokondee <suraphan.l@kaidee.com>
2016-11-22 23:38:18 +0700

    Initial Commit

---
+++ ImageScaling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageScaling
+//
+//  Created by Suraphan Laokondee on 11/22/2559 BE.
+//  Copyright © 2559 Suraphan Laokondee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
